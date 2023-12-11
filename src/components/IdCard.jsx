d73f157c52d771dbb9966d083013736510199442
mirJakub <slawomir.krzyzak@gmail.com>
2023-12-11 18:35:16 +0100

    test

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,16 @@
+import App from "../App";
+
+function idCard(obj) {
+    return (
+        <div className="singleCard">
+            <div className="pic"><img src={obj.picture} alt="" srcset="" /></div>
+           <div className="objData">
+             <div><b>First Name:</b> {obj.firstName}</div>
+            <div><b>Last Name: </b>F{obj.lastName}</div>
+            <div><b>Gender: </b>F{obj.gender}</div>
+            </div>
+
+        </div>
+    )
+}
+export default idCard;
